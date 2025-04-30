11acdb1a8d14983c36522664e8aa08ed2d87d20d
sam-msds <112344802+sam-msds@users.noreply.github.com>
2025-04-30 14:52:50 -0700

    Changed scripts to update config loctaion 2

--- scripts/run_all_configs.py
+++ scripts/run_all_configs.py
@@ -3,7 +3,7 @@ import subprocess
 base_filename = "/content/drive/MyDrive/CS598DHL_RawFiles/CS598DLH_Project/keyclass/scripts/config_mimic_"
 base_file_extension = '.yaml'
 
-for i in range(0,19):
+for i in range(0,0):
     filename = base_filename + str(i) + base_file_extension
     print("Running ",filename)
     subprocess.run(["python","run_all.py","--config",filename])
