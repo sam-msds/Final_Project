943388fdbdb54706f957303f4906502442852544
sam-msds <112344802+sam-msds@users.noreply.github.com>
2025-04-30 00:47:59 -0700

    Changed scripts to update config loctaion 2

--- scripts/final_run.sh
+++ scripts/final_run.sh
@@ -10,12 +10,12 @@ cd keyclass/scripts/
 
 mkdir data/
 cd data/
-FILE_ID="1eK0DS8NdCOev4lJKggpMrzD_AzGNm4eN" #Full MIMIC DATASET
+FILE_ID="1idGFVRc5tfi_LyGihjQBnxB_9t4G0DlD" #Full MIMIC DATASET
 #FILE_ID="1DaXCjzs8I4PCpzaldaDNIublK4MedD-_" #10 MIMIC DATASET
-URL="https://drive.google.com/uc?export=download&id=$FILE_ID"
-#echo ${green}===Downloading MIMIC Data...===${reset}
-#wget --load-cookies /tmp/cookies.txt "https://drive.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate $URL -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILE_ID" -O "mimic.zip" && rm -rf /tmp/cookies.txt
-#echo ${green}===Unzipping MIMIC Data...===${reset}
+URL="https://docs.google.com/uc?export=download&id=$FILE_ID"
+echo ${green}===Downloading MIMIC Data...===${reset}
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate $URL -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=$FILE_ID" -O "mimic.zip" && rm -rf /tmp/cookies.txt
+echo ${green}===Unzipping MIMIC Data...===${reset}
 jar xvf mimic.zip && rm mimic.zip
 #mv small-mimic mimic
 
@@ -23,6 +23,6 @@ cd -- "$BASE_PATH"
 cd CS598DLH_Project/keyclass/scripts/
 python config_creator.py
 python run_all_configs.py
-CONFIG_LOCATION="${BASE_PATH}CS598DLH_Project/keyclass/config_files/config_mimic.yaml"
+CONFIG_LOCATION="${BASE_PATH}CS598DLH_Project/config_files/config_mimic.yaml"
 echo $CONFIG_LOCATION
 python run_all.py --config $CONFIG_LOCATION
