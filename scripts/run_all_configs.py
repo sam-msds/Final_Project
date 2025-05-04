3d449e7952dda5628f077b83f990726c2c97d11b
sam-msds <112344802+sam-msds@users.noreply.github.com>
2025-05-04 00:44:21 -0700

    Change to 20 sample and larger subset

--- scripts/run_all_configs.py
+++ scripts/run_all_configs.py
@@ -3,7 +3,7 @@ import subprocess
 base_filename = "/content/drive/MyDrive/CS598DLH_KeyClass_Reproduce/CS598DLH_Project/keyclass/scripts/config_mimic_"
 base_file_extension = '.yaml'
 
-for i in range(0,5):
+for i in range(0,10):
     filename = base_filename + str(i) + base_file_extension
     print("Running ",filename)
     subprocess.run(["python","run_all.py","--config",filename])
