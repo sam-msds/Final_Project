5639b9db594e7c501f752ddcb3467af16bdc1d9e
sam-msds <112344802+sam-msds@users.noreply.github.com>
2025-05-03 17:21:15 -0700

    Change project path

--- scripts/config_creator.py
+++ scripts/config_creator.py
@@ -2,7 +2,7 @@ import yaml
 
 with open('base_config_mimic.yaml','r') as f:
     contents = yaml.load(f, Loader=yaml.Loader)
-    for i in range(0,10):
+    for i in range(0,5):
         contents['n_class_being_tested'] = i
         newFileName = "config_mimic_"+str(i)+".yaml"
         with open(newFileName,'w') as outputFile:
