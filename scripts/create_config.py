cd817a660bcd01ddacf0bfb6f15b5af8cf45185f
sam-msds <112344802+sam-msds@users.noreply.github.com>
2025-05-07 12:16:36 -0700

    Upload Final Files

--- scripts/create_config.py
+++ scripts/create_config.py
@@ -2,7 +2,7 @@ import yaml
 
 with open('custom_config.yaml','r') as f:
     contents = yaml.load(f, Loader=yaml.Loader)
-    for i in range(0,20):
+    for i in range(0,19):
         contents['n_class_custom'] = i
         newFileName = "custom_config_class"+str(i)+".yaml"
         with open(newFileName,'w') as outputFile:
